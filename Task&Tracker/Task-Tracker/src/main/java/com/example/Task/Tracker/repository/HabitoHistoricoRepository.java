9675f68ad102ff3647406dda8a633566326de87d
Lucas Pessoli <pessolilucas@gmail.com>
2024-09-24 21:44:22 -0300

    Delete Task&Tracker/Task-Tracker/src/main/java/com/example/Task/Tracker directory

--- Task&Tracker/Task-Tracker/src/main/java/com/example/Task/Tracker/repository/HabitoHistoricoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Task.Tracker.repository;
-
-import com.example.Task.Tracker.model.HabitosHistoricos;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HabitoHistoricoRepository extends JpaRepository<HabitosHistoricos, Long> {
-}
